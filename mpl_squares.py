ee83c6006ad1eba2935d0ca2def7d904a7f95dba
deverser <alexmal74@gmail.com>
2018-12-06 00:03:58 +0500

    added input values, tuned plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(input_values, squares, linewidth=5)
+# Назначение заголовка программы и меток осей
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+# Назначение размера шрифта делений на осях.
+plt.tick_params(axis='both', labelsize=14)
 plt.show()
