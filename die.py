639050ace1ebc711862dfc0d69b305550592b2df
deverser <alexmal74@gmail.com>
2019-01-17 02:25:13 +0500

    die roll visualization using pygal library

---
+++ die.py
@@ -0,0 +1,13 @@
+﻿from random import randint
+
+
+class Die():
+    """Класс, представляющий кубик"""
+    def __init__(self, num_sides=6):
+        """По умолчанию кубик шестигранный"""
+        self.num_sides = num_sides
+
+    def roll(self):
+        """Возвращает случайное число от 1 до числа граней"""
+        return randint(1, self.num_sides)
+    
