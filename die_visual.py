772353ee6cf8b0caa029f9de09de911c90ec89e6
deverser <alexmal74@gmail.com>
2019-01-17 18:06:07 +0500

    refactoring of die_visual.py (added list generators instead cycles) added dice_visual.py and different_dice.py for rolling two dice (D6 + D6) and D6 + D10

--- die_visual.py
+++ die_visual.py
@@ -6,23 +6,17 @@ import pygal
 die = Die()
 
 # Моделирование серии бросков с сохранением результатов в списке
-results = []
-for roll_num in range(1000):
-    result = die.roll()
-    results.append(result)
+results = [die.roll() for roll_num in range(1000)]
 
 # print(results)
 # Анализ результатов
-frequencies = []
-for value in range(1, die.num_sides+1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(1, die.num_sides+1)]
 
 # print(frequencies)
 # Визуализация результатов
 hist = pygal.Bar()
 hist.title = 'Results of rolling one D6 1000 times'
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_labels = [str(x) for x in range(1, die.num_sides+1)]
 hist.x_title = 'Result'
 hist.y_title = 'Frequency of result'
 
